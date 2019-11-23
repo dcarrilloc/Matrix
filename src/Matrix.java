6af4752aba3edfcdd2d11443fb243000f2d64127
Home-danielcarrillocardus <danielcarrillocardus@gmail.com>
2019-11-23 12:01:32 +0100

    Añadiendo comentarios en Matrix.java

--- src/Matrix.java
+++ src/Matrix.java
@@ -3,9 +3,10 @@ public class Matrix {
 
     /**
      * Agafarem, amb el bucle for, els elements de la
-     * diagonal de la matriu i els sumarem a la vaiable result.
-     * @param mat array amb la que farem feina
-     * @return double suma de la diagonal de la matriu
+     * diagonal de la matriu i els sumarem a la variable result.
+     *
+     * @param mat array bidimensional de la matriu
+     * @return diagonal de la matriu
      */
     static double trace(double[][] mat) {
 
@@ -22,16 +23,18 @@ public class Matrix {
     }
 
     /**
-     * @param mat1 sds
-     * @param mat2  sdsd
-     * @return sds
+     * Crearem un array bidimensional (result[][]) on guardar la
+     * suma de les matrius (mat1 i mat2).
+     * Amb els fors recorrerem les posicions de mat1
+     * i mat2 i les guardarem a result.
+     *
+     * @param mat1 matriu 1
+     * @param mat2  matriu 2
+     * @return matriu resultant de la suma
      */
     static double[][] add(double[][] mat1, double[][] mat2) {
         /*
-         Crearem un array bidimensional (result[][]) on guardar la
-         suma dels dos arrays importats (mat1 i mat2).
-         Amb els fors recorrerem les posicions de mat1
-         i mat2 i les guardarem a result.
+
          */
 
         if (!matrixExists(mat1) || !matrixExists(mat2) || !testSameDimension(mat1, mat2)) {
@@ -50,12 +53,15 @@ public class Matrix {
     }
 
     /**
-     * @param mat1
-     * @param mat2
-     * @return
+     * Aquesta funció realitza el producte de dos matrius
+     *
+     * @param mat1 matriu 1
+     * @param mat2 matriu 2
+     * @return matriu resultant de fer el producte
      */
     static double[][] mult(double[][] mat1, double[][] mat2) {
 
+        // comprovació de que les matrius existeixen i de que es pot fer la multiplicació
         if (!matrixExists(mat1) || !matrixExists(mat2) || mat1[0].length != mat2.length) {
             return null;
         }
@@ -75,9 +81,11 @@ public class Matrix {
     }
 
     /**
-     * @param mat
-     * @param p
-     * @return
+     * Aquesta funció realitza la potència d'una matriu.
+     *
+     * @param mat matriu que elevarem.
+     * @param p exponent de la potència.
+     * @return matriu resultant de realitzar la potència.
      */
     static double[][] power(double[][] mat, int p) {
 
@@ -92,7 +100,7 @@ public class Matrix {
             return result;
         }
 
-        // Bucle per copiar la matriu "mat" dins la matriu amb la que farem feina més endavant.
+        // Bucle per copiar la matriu "mat" dins la matriu result amb la que farem feina més endavant
         for (int i = 0; i < mat.length; i++) {
             System.arraycopy(mat[i], 0, result[i], 0, mat[0].length);
         }
@@ -105,9 +113,13 @@ public class Matrix {
     }
 
     /**
-     * @param mat1
-     * @param mat2
-     * @return
+     * Aquesta funció realitza la divisió entre dues matrius.
+     * Com que matemàticament no es pot fer la divisió de dues matrius
+     * podem multiplicar la primera matriu per la inversa de la segona.
+     *
+     * @param mat1 matriu 1
+     * @param mat2 matriu 2
+     * @return el resultat de fer la divisió de les dues matrius
      */
     static double[][] div(double[][] mat1, double[][] mat2) {
 
@@ -123,12 +135,16 @@ public class Matrix {
     }
 
     /**
-     * @param mat
-     * @param x1
-     * @param y1
-     * @param x2
-     * @param y2
-     * @return
+     * A través de les coordenades (x1,y1), (x2,y2) podem extreure una submatriu d'una matriu més gran.
+     * Agafarem tots els elements de la matriu original que estiguin dintre del requadre imaginari
+     * creat per les coordenades i els afegirem a la matriu resultant.
+     *
+     * @param mat matriu
+     * @param x1 primer element de la coordenada 1
+     * @param y1 segon element de la coordenada 1
+     * @param x2 primer element de la coordenada 2
+     * @param y2 segon element de la coordenada 2
+     * @return matriu resultant de calcular la submatriu de la matriu principal
      */
     static double[][] submatrix(double[][] mat, int x1, int y1, int x2, int y2) {
 
@@ -179,10 +195,11 @@ public class Matrix {
 
     /**
      * En aquesta funció multiplicarem el valor de la posició
-     * de la matriu entrar per l'escalar importat.
-     * @param mat
-     * @param n
-     * @return
+     * de la matriu per un escalar.
+     *
+     * @param mat matriu
+     * @param n nombre escalar
+     * @return matriu resultant de multiplicar una matriu per un escalar.
      */
     static double[][] mult(double[][] mat, double n) {
 
@@ -202,8 +219,9 @@ public class Matrix {
     }
 
     /**
-     * @param mat
-     * @return
+     * Aquesta funció retorna l'inversa d'una matriu.
+     * @param mat matriu
+     * @return matriu invertida
      */
     static double[][] invert(double[][] mat) {
 
@@ -223,6 +241,7 @@ public class Matrix {
         // copiam la matriu importada a una local per manipularla
         System.arraycopy(mat, 0, aux, 0, mat.length);
 
+        // sustituim cada element de la matriu per el determinant del seu adjunt.
         for (int i = 0; i < result.length; i++) {
             for (int j = 0; j < result[0].length; j++) {
                 result[i][j] = (Math.pow(-1, i + j)) * determinant(getMinor(aux, i, j));
@@ -238,11 +257,11 @@ public class Matrix {
 
     /**
      * Aquesta funció retorna una matriu sense la fila i
-     * la columna del valor amb coordenades x i y.
-     * @param mat dfd
-     * @param x dfdf
-     * @param y dfd
-     * @return dfd
+     * la columna del valor amb coordenades x i y. És a dir, el seu adjunt.
+     * @param mat matriu
+     * @param x coordenada x
+     * @param y coordenada y
+     * @return matriu adjunta
      */
     static double[][] getMinor(double[][] mat, int x, int y) {
 
@@ -274,8 +293,10 @@ public class Matrix {
     }
 
     /**
-     * @param mat
-     * @return
+     * Aquesta funció calcula el determinant d'una funció. en aquest cas emprarem el métode
+     * de la recursió per a calcular determinants de dimensió qualsevol.
+     * @param mat matriu
+     * @return deterrminant de la matriu
      */
     static double determinant(double[][] mat) {
 
@@ -307,14 +328,14 @@ public class Matrix {
     }
 
     /**
-     * @param mat
-     * @return result
+     * Aquesta funció calculla transposada d'una funció. Crearem un array bidimensional (result[][])
+     * on guardarem els valors de les posicions de la matriu però amb els indexs girats.
+     * Per exemple: result[2][3] = mat[3][2].
+     *
+     * @param mat matriu
+     * @return matriu transposada
      */
     static double[][] transpose(double[][] mat) {
-        /*
-         Crearem un array bidimensional (result[][]) on guardarem els valors de les posicions de l'array importat
-         però amb els indexs girats. Per exemple: result[2][3] = mat[3][2].
-         */
 
         if (!matrixExists(mat)) {
             return null;
@@ -332,8 +353,11 @@ public class Matrix {
     }
 
     /**
-     * @param mat
-     * @return
+     * Aquesta funció retorna un boolean (true / false) segons si la matriu es considera ortogonal.
+     * Una matriu és ortogonal si la seva transposada és igual a la seva inversa.
+     *
+     * @param mat matriu
+     * @return true si és ortogonal / false si NO és ortogonal
      */
     static boolean isOrtho(double[][] mat) {
 
@@ -343,6 +367,12 @@ public class Matrix {
 
         double[][] trans = transpose(mat);
         double[][] inv = invert(mat);
+
+        /*
+         anam comparant element per element de les dues matrius i els restam per saber si
+         el valor és el mateix. Decidim l'index d'error, en aquest cas 0.0001, és un delta
+         bastant precís.
+         */
 
         for (int i = 0; i < mat.length; i++) {
             for (int j = 0; j < mat.length; j++) {
@@ -358,8 +388,10 @@ public class Matrix {
     }
 
     /**
-     * @param mat
-     * @return
+     * En aquesta funció implementarem la Regla de Cramer per a la resolució de sistemes d'equacions.
+     *
+     * @param mat matriu
+     * @return array amb els valors de les incògnites [x,y,z,...]
      */
     static double[] cramer(double[][] mat) {
 
@@ -408,6 +440,12 @@ public class Matrix {
         return result;
     }
 
+    /**
+     * Aquesta funció retorna la matriu unitària
+     *
+     * @param mat matriu necesitada per saber la dimensió que tendrà la matriu unitària
+     * @return matriu unitària
+     */
     private static double[][] matriuUnitaria(double[][] mat) {
 
         double[][] matriuUnitaria = new double[mat.length][mat[0].length];
@@ -425,9 +463,15 @@ public class Matrix {
         return matriuUnitaria;
     }
 
+    /**
+     * Aquesta funció retorna un boolean segons si les dues matrius tenen la mateixa dimensió
+     *
+     * @param mat1 matriu 1
+     * @param mat2 matriu 2
+     * @return true si tenen la mateixa dimensió / false si no
+     */
     private static boolean testSameDimension(double mat1[][], double mat2[][]) {
 
-        // comprovar que tenguin la mateixa dimensió
         if (mat1[0].length != mat2[0].length || mat1.length != mat2.length) {
             return false;
         }
@@ -435,6 +479,13 @@ public class Matrix {
         return true;
     }
 
+    /**
+     * Aquesta funció ens diu en forma de boolean si una matriu és quadrada. És a dir, si té
+     * el mateix nombre de files que de columnes.
+     *
+     * @param mat matriu
+     * @return true si es quadrada / false si no
+     */
     private static boolean squareMatrix(double mat[][]) {
 
         if (mat.length != mat[0].length) {
@@ -444,6 +495,12 @@ public class Matrix {
 
     }
 
+    /**
+     * En aquesta funció ens asseguram que una matriu existi i sigui vàlida per fer feina amb ella.
+     *
+     * @param mat matriu
+     * @return true si existeix / false si no
+     */
     private static boolean matrixExists(double mat[][]) {
 
         if (Double.isNaN(mat.length)) {
